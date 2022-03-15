6482af6bea76832980f666df7631e3a4cf30738b
mess_web <meszewde@gmail.com>
2022-03-15 05:51:11 -0700

    add main file

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,24 +1,21 @@
 #include "main.h"
 
 /**
- * main - check the code for ALX School students.
- *
- * Program that prints Holberton, followed by a new line
- *
- * Return: always 0 (success)
+ * main -  prints Holberton, followed by a new line
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	char s[] = "_putchar";
-	int i = 0;
-
-	while (s[i] != '\0')
-	{
-		_putchar(s[i]);
-		i++;
-	}
-	_putchar(10);
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
 
 	return (0);
 }
